0386ca33290864091ea6ea66a08274cad194c971
johnlindsey829 <150502127+johnlindsey829@users.noreply.github.com>
2023-11-10 23:54:43 -0500

    Add files via upload

---
+++ age_test.py
@@ -0,0 +1,6 @@
+# Test to determine if at least 21 
+age = int(input("Enter your age:")) 
+if age >= 21: 
+    print("You are old enough to buy alcohol or tobacco.") 
+else: 
+    print("You are a minor.") 
